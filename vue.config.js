296ddc4a671031195dd7ca35159570b5ebc40bde
sakhura <sabina.romero@gmail.com>
2024-12-16 20:51:12 -0300

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': { // Todas las llamadas a '/api' pasarán por aquí
+        target: 'https://github.com/Sakhura/clase8', // Dirección del servidor
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' } // Opcional: reescribe la ruta
+      }
+    }
+  }
+};
