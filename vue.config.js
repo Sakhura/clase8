b4ce9d74c9872415152d7c8105d6acefd40793cc
sakhura <sabina.romero@gmail.com>
2024-12-16 20:52:29 -0300

    update date servidor

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': { // Todas las llamadas a '/api' pasarán por aquí
-        target: 'https://github.com/Sakhura/clase8', // Dirección del servidor
+        target: 'https://sakhura.github.io/clase8/', // Dirección del servidor
         changeOrigin: true,
         pathRewrite: { '^/api': '' } // Opcional: reescribe la ruta
       }
