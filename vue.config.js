fbe29dfb5d85105e58a87ab7994250764779c2fd
sakhura <sabina.romero@gmail.com>
2024-12-16 21:11:55 -0300

    update link

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/Sakhura/clase8/'
+    ? '/clase8/'
     : '/'
 })
  
